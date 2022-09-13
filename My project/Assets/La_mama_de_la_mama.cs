555a185ab7c3e0c64b86e51326b7b4b90d2a6f62
makarr0n <113384137+makarr0n@users.noreply.github.com>
2022-09-13 23:09:33 +0200

    D I A B L O

---
+++ My project/Assets/La_mama_de_la_mama.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class La_mama_de_la_mama : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        //Nasheeeeee
+    }
+}
